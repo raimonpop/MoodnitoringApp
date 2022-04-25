9f091b3478071a00338f6e9193d147f5846838d0
raimonvelasco <raimonvelasco6@gmail.com>
2022-04-25 22:05:54 +0200

    Firebase auth UI

--- app/src/main/java/raimon/example/moodnitoringapp/MainActivity.kt
+++ app/src/main/java/raimon/example/moodnitoringapp/MainActivity.kt
@@ -1,11 +1,88 @@
 package raimon.example.moodnitoringapp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private lateinit var authStateListener: FirebaseAuth.AuthStateListener
+
+    private val resultLauncher= registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        val response = IdpResponse.fromResultIntent(it.data)
+
+        if(it.resultCode == RESULT_OK){
+            val user = FirebaseAuth.getInstance().currentUser
+            if(user != null){
+                Toast.makeText(this, "Bienvenido", Toast.LENGTH_SHORT).show()
+            }else {
+                if (response == null){
+                    Toast.makeText(this,"Hasta luego", Toast.LENGTH_SHORT).show()
+                    finish()
+                }else{
+                    response.error?.let {
+                        if(it.errorCode == ErrorCodes.NO_NETWORK){
+                            Toast.makeText(this,"No hay conexion a internet", Toast.LENGTH_SHORT).show()
+                        }else{
+                            Toast.makeText(this,"Codigo de eroor: ${it.errorCode}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            }
+
+
+        }
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        configureAuth()
+    }
+
+    private fun configureAuth() {
+
+        firebaseAuth = FirebaseAuth.getInstance()
+        authStateListener = FirebaseAuth.AuthStateListener { auth ->
+            if (auth.currentUser != null){
+               supportActionBar?.title = auth.currentUser?.displayName
+            }else{
+                val provider = arrayListOf(
+                    AuthUI.IdpConfig.EmailBuilder().build(),
+                    //AuthUI.IdpConfig.GoogleBuilder().build(),
+                    //AuthUI.IdpConfig.FacebookBuilder().build(),
+                    //AuthUI.IdpConfig.PhoneBuilder().build()
+                )
+
+               resultLauncher.launch(AuthUI.getInstance()
+                   .createSignInIntentBuilder()
+                   .setIsSmartLockEnabled(false)
+                   .setAvailableProviders(provider)
+                   .build())
+
+
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        firebaseAuth.addAuthStateListener(authStateListener)
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        firebaseAuth.removeAuthStateListener(authStateListener)
+
     }
 }
