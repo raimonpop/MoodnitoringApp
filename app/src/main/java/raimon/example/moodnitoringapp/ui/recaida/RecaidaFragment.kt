9104e9a5029c1e87ece9781147dc8566d6ce9944
velasco.carbonell.raimon <velasco.carbonell.raimon@alumnat.copernic.cat>
2022-05-13 15:03:10 +0200

    recaida acabado

--- app/src/main/java/raimon/example/moodnitoringapp/ui/recaida/RecaidaFragment.kt
+++ app/src/main/java/raimon/example/moodnitoringapp/ui/recaida/RecaidaFragment.kt
@@ -5,16 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import raimon.example.moodnitoringapp.Constants
 import raimon.example.moodnitoringapp.R
 import raimon.example.moodnitoringapp.databinding.FragmentRecaidaBinding
+import raimon.example.moodnitoringapp.model.Notes
 import raimon.example.moodnitoringapp.ui.recaida.database.DatabaseHelper
 
 
 class RecaidaFragment : Fragment() {
     private var binding: FragmentRecaidaBinding? = null
-    private lateinit var database: DatabaseHelper
     private lateinit var hipoAdapter: RecaidaAdapter
     private lateinit var depreAdapter: RecaidaAdapter
+    private lateinit var database: DatabaseHelper
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,7 +27,98 @@ class RecaidaFragment : Fragment() {
         binding?.let {
             return it.root
         }
+
         // Inflate the layout for this fragment
         return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        database = DatabaseHelper(this.requireContext())
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerViewHipo()
+        setupRecyclerViewDepre()
+        setupBottonHipo()
+        setupBottonDepre()
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        getData()
+    }
+
+
+     fun getData() {
+        val data = database.getAllNotes()
+        data.forEach { note ->
+            addNoteAuto(note)
+        }
+    }
+
+    private fun setupBottonDepre() {
+        binding?.let { binding ->
+            binding.btnAddDepre.setOnClickListener {
+                if(binding.etDepre.text.toString().isNotBlank()){
+                    val note = Notes(text = binding.etDepre.text.toString(), hipo_depr = false)
+                    note.id = database.inserNote(note)
+                    if (note.id != Constants.ID_ERROR){
+                        addNoteAuto(note)
+                        binding.etDepre.text?.clear()
+
+                    }
+                }else{
+                    binding.etDepre.error = "Campo requerido"
+                }
+            }
+        }
+    }
+
+    private fun addNoteAuto(note: Notes) {
+        if(note.hipo_depr){
+            hipoAdapter.add(note)
+        }else{
+            depreAdapter.add(note)
+        }
+    }
+
+    private fun setupBottonHipo() {
+        binding?.let { binding ->
+            binding.btnAddHipo.setOnClickListener {
+                if(binding.etHipo.text.toString().isNotBlank()){
+                    val note = Notes(text = binding.etHipo.text.toString(), hipo_depr = true)
+                    note.id = database.inserNote(note)
+                    if (note.id != Constants.ID_ERROR){
+                        addNoteAuto(note)
+                        binding.etHipo.text?.clear()
+                    }
+                }else{
+                    binding.etHipo.error = "Campo requerido"
+                }
+            }
+        }
+    }
+
+    private fun setupRecyclerViewDepre() {
+        depreAdapter = RecaidaAdapter(mutableListOf())
+        binding?.let {
+            it.rvDepre.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = this@RecaidaFragment.depreAdapter
+            }
+        }
+    }
+
+    private fun setupRecyclerViewHipo() {
+        hipoAdapter = RecaidaAdapter(mutableListOf())
+        binding?.let {
+            it.rvHipo.apply {
+                layoutManager = LinearLayoutManager(context)
+                adapter = this@RecaidaFragment.hipoAdapter
+            }
+        }
     }
 }
